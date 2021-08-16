b9291424afe52068521212f622e0fc6b2cd7ab2f
Nitin V3muri <nitinvemuri@gmail.com>
2021-08-16 03:30:25 -0700

    changing some mutations and queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,20 +1,20 @@
 import gql from 'graphql-tag';
 
 export const GET_ME = gql`
-{
-    me{
-        _id
-        username
-        email
-        bookCount
-        savedBooks{
-            # _id
-            description
-            title
-            bookId
-            image
-            link
-            authors
-        }
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks{
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
     }
-}`
+  }
+`;
